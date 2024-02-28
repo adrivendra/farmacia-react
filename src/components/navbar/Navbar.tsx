7edacb52c18cd13957cb649e6f73719876c444f3
Adriano <adrianovendramini@hotmail.com>
2024-02-28 09:29:46 -0300

    navbar e footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+function Navbar() {
+  return (
+    <>
+      <div className="w-full bg-indigo-900 text-white flex justify-center py-4">
+        <div className="container flex justify-between text-lg">
+          <div className="text-2xl font-bold uppercase">
+            Farmacia do Adriano
+          </div>
+
+          <div className="flex gap-4">
+            <div className="hover:underline">Produtos</div>
+            <div className="hover:underline">Categorias</div>
+            <div className="hover:underline">Cadastrar Categoria</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
